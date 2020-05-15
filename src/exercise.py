bb8ce33779633fa373151a715bf6f6afc57be894
cabosh <callum.bosher@gmail.com>
2020-05-15 11:52:06 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
-    #write your code below this line
+
+    days = int(input("How many days would you like to convert into seconds? "))
+
+    seconds = (days * 86400)
+
+    print("There are " + str(seconds) + " seconds in " + str(days) + " days.")
 
 if __name__ == '__main__':
     main()
